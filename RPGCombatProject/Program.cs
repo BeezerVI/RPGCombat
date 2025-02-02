3ef39b8127e32a03eac68a260300bdf41cb02df9
BeezerVI <hyrummckay@gmail.com>
2025-02-01 17:10:03 -0800

    by brain herts

--- RPGCombatProject/Program.cs
+++ RPGCombatProject/Program.cs
@@ -6,7 +6,7 @@ using GameState = RPGCombatProject.GameLogic.GameState;
 
 namespace RPGCombatProject
 {
-    public class Program //test
+    public class Program
     {
         public static void Main(string[] args)
         {
